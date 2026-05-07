7cc844c9ef200f0a5c8b94f094493541dda32d82
SyahidHalid <syahidhalid@gmail.com>
2026-05-07 11:27:16 +0800

    .

--- SR2604-18803 PD at Origination Migration.py
+++ SR2604-18803 PD at Origination Migration.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 import pyodbc
@@ -10,9 +11,12 @@ link = r"C:\\Users\\syahidhalid\\Syahid_PC\Analytics - ITD\\00. URS & Test Scrip
 
 file = "Loandatabase_Feb2026 (Rating) (ITSD)"
 
+#sheet_name = "Loandatabase_Feb2026"
+sheet_name = "Loandatabase_Feb2026 (UAT)"
+
 filepath = os.path.join(link, file + ".xlsx")
 
-Excel = pd.read_excel(filepath,header=5, usecols="B:FH")
+Excel = pd.read_excel(filepath,header=5, usecols="B:FH", sheet_name=sheet_name)
 
 Excel1 = Excel.iloc[np.where(Excel['Status of Account'].isin(['Active',
                                                               'Active-Overdue',
@@ -38,7 +42,11 @@ Excel2 = Excel1[["Exim Account Number/ FA Number",
                  "Position as At"]]
 #  Excel2['Exim Account Number/ FA Number'].value_counts()
 
-# UAT
+Excel2["AccountNumber"] = Excel2["Exim Account Number/ FA Number"].str.replace("-", "", regex=False)
+#  Excel2["AccountNumber"].value_counts()
+
+
+#  UAT
 connection = pyodbc.connect(
    'DRIVER={ODBC Driver 17 for SQL Server};'
    'SERVER=10.32.1.52,1455;'
@@ -48,20 +56,83 @@ connection = pyodbc.connect(
    'Encrypt=yes;TrustServerCertificate=yes')  # Use if you encounter SSL issues
 # amik kat application_master EOD bru dia update col_facilities application master
 
-#  PROD Connect Database
-connection = pyodbc.connect(
-   'DRIVER={ODBC Driver 17 for SQL Server};'
-   'SERVER=10.20.1.4,1455;'
-   'DATABASE=mis_db_prod;'
-   'UID=efms_admin;'
-   'PWD=Exim1234;'
-   'Encrypt=yes;TrustServerCertificate=yes')  # Use if you encounter SSL issues
+# #  PROD Connect Database
+# connection = pyodbc.connect(
+#    'DRIVER={ODBC Driver 17 for SQL Server};'
+#    'SERVER=10.20.1.4,1455;'
+#    'DATABASE=mis_db_prod;'
+#    'UID=efms_admin;'
+#    'PWD=Exim1234;'
+#    'Encrypt=yes;TrustServerCertificate=yes')  # Use if you encounter SSL issues
 
-LDB_FTM = pd.read_sql_query("SELECT * FROM col_facilities_application_master;", connection)
 
-LDB_FTM1 = LDB_FTM[['facility_exim_account_num','acc_rating_origination']]
+# LDB_CA = pd.read_sql_query("SELECT * FROM application_master;", connection)
+#     LDB_CA['application_id'].value_counts()   primary key
+#     LDB_CA['cif_id'].value_counts()
 
-# LDB_FTM[['facility_exim_account_num']].value_counts()
+
+LDB_ACC = pd.read_sql_query("SELECT * FROM col_facilities_application_master;", connection)
+#     LDB_ACC['application_id'].value_counts() secondary
+#     LDB_ACC['facility_id'].value_counts() primary
+#     LDB_ACC['cif_id'].value_counts()
+#     LDB_ACC['facility_exim_account_num'].value_counts()
+
+
+Excel3 = Excel2[["AccountNumber","Rating at Origination","Position as At"]].merge(LDB_ACC[['facility_exim_account_num','acc_rating_origination','application_id']].rename(columns={'facility_exim_account_num':'AccountNumber'}), on='AccountNumber', how='left', indicator=True)
+#     Excel2.shape
+#     LDB_ACC.shape
+#     Excel3.shape
+#     Excel3._merge.value_counts()
+#     Excel3.application_id.value_counts()
+reportingDate = "Position as At"
+
+column_types = []
+for col in Excel3.columns:
+   # You can choose to map column types based on data types in the DataFrame, for example:
+   if Excel3[col].dtype == 'object':  # String data type
+         column_types.append(f"{col} VARCHAR(255)")
+   elif Excel3[col].dtype == 'int64':  # Integer data type
+         column_types.append(f"{col} INT")
+   elif Excel3[col].dtype == 'float64':  # Float data type
+         column_types.append(f"{col} FLOAT")
+   else:
+         column_types.append(f"{col} VARCHAR(255)")  # Default type for others
+
+cursor.execute("DROP TABLE IF EXISTS A_MAI_PD_RATING")
+connection.commit()
+
+# Generate the CREATE TABLE statement
+create_table_query = "CREATE TABLE A_MAI_PD_RATING (" + ', '.join(column_types) + ")"
+# Execute the query
+cursor.execute(create_table_query)
+
+for row in Excel3.iterrows():
+   sql = "INSERT INTO A_MAI_PD_RATING({}) VALUES ({})".format(','.join(Excel3.columns), ','.join(['?']*len(appendfinal1.columns)))
+   cursor.execute(sql, tuple(row[1]))
+connection.commit()
+
+
+cursor.execute("""MERGE INTO application_master AS target USING A_MAI_PD_RATING AS source
+ON target.application_id = source.application_id
+WHEN MATCHED AND target.position_as_at = ? THEN
+   UPDATE SET target.acc_credit_loss_laf_ecl = source.LAF_ECL_FC,
+      target.acc_credit_loss_laf_ecl_myr = source.LAF_ECL_MYR,
+      target.acc_credit_loss_cnc_ecl = source.CnC_ECL_FC,
+      target.acc_credit_loss_cnc_ecl_myr = source.CnC_ECL_MYR,
+      target.acc_credit_loss_acc_receiv_ecl = source.AR_ECL_FC,
+      target.acc_credit_loss_acc_receiv_ecl_myr = source.AR_ECL_MYR;
+""", (reportingDate,))
+conn.commit() 
+
+cursor.execute("drop table A_MAI_PD_RATING")
+conn.commit() 
+
+
+
+
+
+
+
 
 LDB_Hist = pd.read_sql_query("SELECT * FROM dbase_account_hist WHERE position_as_at = (SELECT MAX(position_as_at) FROM dbase_account_hist);", connection)
 
